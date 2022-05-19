aeb1fb129f01d42777e022d2384dea7c830951b3
Malte Jörn Ziebarth <malte.ziebarth@fmvkb.de>
2022-05-19 15:42:13 +0200

    Add missing header patch.

--- include/paths.hpp
+++ include/paths.hpp
@@ -13,7 +13,8 @@ namespace projplot {
 std::vector<path_xy_t>
 crop_and_refine(const path_geo_t& geo_path, const ProjWrapper& proj,
                 double xmin, double xmax, double ymin, double ymax,
-                double bisection_offset, double minimum_node_distance);
+                double bisection_offset, double minimum_node_distance,
+                double cut_at_angle_degrees, bool cyclic);
 
 }
 
