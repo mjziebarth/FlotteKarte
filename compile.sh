506a698ff3b51c62d9a8a8dea7d4f0e041ebebd7
Malte J. Ziebarth <ziebarth@gfz-potsdam.de>
2022-08-05 13:48:25 +0200

    Prepare for supplying recompilation facilities by including the C++ source in the installed Python project tree.

--- compile.sh
+++ compile.sh
@@ -13,4 +13,24 @@ meson compile -C builddir
 if [ ! -f flottekarte/extensions/libflottekarte.so ]; then
     cd flottekarte/extensions
     ln -s ../../builddir/libflottekarte.so
+    cd ../..
+fi
+
+# Link the C++ source code into the Python package tree
+# so as to be able to potentially recompile the extension
+# from within the package:
+if [ ! -d flottekarte/extensions/include/ ]; then
+    cd flottekarte/extensions/
+    ln -s ../../include
+    cd ../..
+fi
+if [ ! -d flottekarte/extensions/src/ ]; then
+    cd flottekarte/extensions/
+    ln -s ../../src
+    cd ../..
+fi
+if [ ! -f flottekarte/extensions/meson.build ]; then
+    cd flottekarte/extensions
+    ln -s ../../meson.build
+    cd ../..
 fi
